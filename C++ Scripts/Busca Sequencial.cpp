cab4455530d81ba6a87e905bc024d8982df2c96f
Isaque Hollanda Gonçalves <77072103+Isaquehg@users.noreply.github.com>
2021-08-11 14:48:10 -0300

    C203 Class

---
+++ C++ Scripts/Busca Sequencial.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int sequencial(int vet[], int tam, int x){//func p retornar posicao no vetor do num
+    bool passouaq = false;//tem o numero no vetor
+    int pos;//posicao no vetor
+    for(int y = 0; y < tam; y ++){
+        if(x == vet[y]){//se achar
+            pos = y;
+            passouaq = true;
+            return pos;
+        }
+        else if(passouaq == false && y == (tam - 1))//se nao achar
+            return -1;
+    }
+}
+int main(){
+    int vet[100];//vetor a ser preenchido
+    int tam = 0;//contador e tamanho do vetor
+    int num;//numero a ser procurado
+    int result;//resultado da funcao
+
+    //input
+    cin >> vet[tam];
+    while(vet[tam] != -1){
+        tam ++;
+        cin >> vet[tam];
+    }
+    cin >> num;
+    //busca
+    result = sequencial(vet, tam, num);
+    //output
+    if(result != -1){
+        cout << num << " encontrado na posicao " << result << endl;
+    }
+    else{
+        cout << num << " nao foi encontrado" << endl;
+    }
+
+    return 0;
+}
