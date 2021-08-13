05b2fee7212e195928f3625c97092354c27ca1ad
Isaque Hollanda Gonçalves <77072103+Isaquehg@users.noreply.github.com>
2021-08-13 09:35:45 -0300

    Update script.js

--- JS Scripts/Web_model/script.js
+++ JS Scripts/Web_model/script.js
@@ -1,1 +1,10 @@
-window.alert('Hi there!')
+/*DEBUGGING:
+Create a breakpoint
+Run
+Start debugging
+type the variable u wanna track
+F10(step over)
+*/
+for(var i = 0; i < 5; i ++){
+    console.log(i)
+}
