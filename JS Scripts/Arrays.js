69f7a9d8cdb3cf5627acdc358d400df977c98ec1
Isaque Hollanda Gonçalves <77072103+Isaquehg@users.noreply.github.com>
2021-08-16 18:48:16 -0300

    js classes

---
+++ JS Scripts/Arrays.js
@@ -0,0 +1,8 @@
+let arr = [];
+let pos = 0;
+for(let i = 0; i < 10; i += 2){
+    console.log(`Number ${i} in position ${pos}`)
+    pos ++;
+}
+//array.push(num) to add new elements
+//PS.: We can use for(pos in num){} in JS!!
---
+++ JS Scripts/Funcions.js
@@ -0,0 +1,9 @@
+function even_odd(num){
+    if(num % 2 == 0){
+        return 'Even!';
+    }
+    else{
+        return 'Odd!';
+    }
+}
+even_odd(4);
