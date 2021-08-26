09bb7e34665e5fbb3ffa96afe7336f912eb6392e
Isaque Hollanda Gonçalves <77072103+Isaquehg@users.noreply.github.com>
2021-08-26 16:32:04 -0300

    Algorithm Sorting and Tree Classes

--- C++ Scripts/Time_Complexity.cpp
+++
@@ -1,9 +0,0 @@
-/*
-melhor caso - Limitante Inferior(omega(n))
-caso medio - teta(n)
-pior caso - Limitante Superior(bigOh) O(n)
-
-T(n) = 7n + 4
-1)Deixar coeficiente iguais a 1:T(n) = n + 1
-2)Manter o maior(seguir tabelinha): O(n)
-*/
